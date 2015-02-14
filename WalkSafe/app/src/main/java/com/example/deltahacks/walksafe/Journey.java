635a03b5893e84ffe4836d73081a927280cec692
JUDY HANWNEN SHEN <judyshen.hw@gmail.com>
2015-02-14 18:03:15 -0500

    Updated Journey Class
    Added some fun stuff

--- WalkSafe/app/src/main/java/com/example/deltahacks/walksafe/Journey.java
+++ WalkSafe/app/src/main/java/com/example/deltahacks/walksafe/Journey.java
@@ -6,19 +6,27 @@ import java.io.Serializable;
  * Created by Judy on 2015-02-14.
  */
 public class Journey implements Serializable{
+    private String start;
+    private String end;
     private String label;
     /**
      * duration should be stored in units of minutes
      */
     private int duration;
 
-    public Journey(String label, int duration) {
-        this.label = label;
+    public Journey(String start, String end, int duration) {
+        this.start = start;
+        this.end = end;
+        this.label = "From " + start + " to " + end;
         this.duration = duration;
     }
 
     public int get_Duration(){
         return duration;
+    }
+
+    public String get_Lable(){
+        return label;
     }
 }
 

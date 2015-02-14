8fdc1d74a38868d6a5ab76c64749788abcea6f9a
JUDY HANWNEN SHEN <judyshen.hw@gmail.com>
2015-02-14 17:35:12 -0500

    Contact Class Addition
    First version of contact class

---
+++ WalkSafe/app/src/main/java/com/example/deltahacks/walksafe/Contact.java
@@ -0,0 +1,19 @@
+package com.example.deltahacks.walksafe;
+
+import java.io.Serializable;
+
+/**
+ * Created by Judy on 2015-02-14.
+ */
+public class Contact implements Serializable{
+    private String name;
+    private String number;
+}
+
+public String getContactName(){
+    return name;
+}
+
+public String getContactNumber(){
+    return "+1" + number;
+}

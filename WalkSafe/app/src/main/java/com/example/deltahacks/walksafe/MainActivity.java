f5116573fc9caf4ece3dff3db3aef9c632c45c35
xofaye <faye@Fayes-MacBook-Pro.local>
2015-02-15 02:03:18 -0500

    updated link view

--- WalkSafe/app/src/main/java/com/example/deltahacks/walksafe/MainActivity.java
+++ WalkSafe/app/src/main/java/com/example/deltahacks/walksafe/MainActivity.java
@@ -57,13 +57,9 @@ public class MainActivity extends Activity {
         }
 
         CustomAdapter adapter = new CustomAdapter(this, R.layout.contact_list_item, contactManager.getContacts());
-<<<<<<< HEAD
-//        ListView listView = (ListView) findViewById(R.id.listView);
-//        listView.setAdapter(adapter);
-=======
-        //ListView listView = (ListView) findViewById(R.id.listView);
-        //listView.setAdapter(adapter);
->>>>>>> FETCH_HEAD
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
     }
 
 

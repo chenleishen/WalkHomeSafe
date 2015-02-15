beb446ee9c334cafa611d989d71f5ba65777eaa9
chenleishen <shenchenlei@hotmail.com>
2015-02-15 05:42:05 -0500

    fixed syntax

--- WalkSafe/app/src/main/java/com/example/deltahacks/walksafe/JourneyActivity.java
+++ WalkSafe/app/src/main/java/com/example/deltahacks/walksafe/JourneyActivity.java
@@ -3,9 +3,17 @@ package com.example.deltahacks.walksafe;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 
 public class JourneyActivity extends ActionBarActivity {
@@ -16,7 +24,7 @@ public class JourneyActivity extends ActionBarActivity {
         @Override
         public void run() {
             checkAgain(v);
-        };
+        }
     };
 
     @Override
@@ -63,16 +71,32 @@ public class JourneyActivity extends ActionBarActivity {
     }
 
     public void helpMe(View view){
-        jane.startPanic(checkAgainSignal);
         Intent intent = getIntent();
-        Intent nextIntent = new Intent(this, HelpActivity.class);
-        startActivity(nextIntent);
+        //    Intent nextIntent = new Intent(this, .class);
+        ContactManager contactManager = (ContactManager) intent.getSerializableExtra("managerKey");
+        ArrayList<Contact> contactList = contactManager.getContacts();
+        String sms = "I have not arrived home at the expected time. Please give me a call ASAP.";
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            for (Contact c: contactList){
+                String phoneNo = c.getContactNumber();
+                smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+            }
+
+            Toast.makeText(getApplicationContext(), "Your friends has been alerted!",
+                    Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS failed!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        //    startActivity(nextIntent);
     }
 
-    public void checkAgain(View view){
+    public void checkAgain(View view) {
         Intent intent = getIntent();
         Intent nextIntent = new Intent(this, WaitActivity.class);
         startActivity(nextIntent);
     }
-
 }
